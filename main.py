b40cb0caf7631e95d86b6b03a108ed93c851c4b2
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-19 22:21:17 +0300

    Добавил свою реализацию функции поиска кратеров

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from scipy.ndimage import label
 import numpy as np
 import csv
+import itertools
 
 
 def read_craters(filename: str, delimiter: str) -> list:
@@ -28,6 +29,7 @@ def calculate(matr: list) -> int:
     larr, num = label(matrxix)
     return num
 
+
 def calculate_selfmade(matr: list) -> int:
     """Функция для подсчета кратеров на поверхности Луны. Функция использует метод Row-by-Row.
 
@@ -38,52 +40,44 @@ def calculate_selfmade(matr: list) -> int:
     Возвращает
       число найденных кратеров
     """
+    if len(matr) == 0 or len(matr[0]) == 0:
+        return 0
     # создание наполненной нулями матрицы ярлыков того же размера что исходная
     labeled_matr = np.zeros((len(matr), len(matr[0])))
     # создание сета для записи конфликтов. Например {(2,1), (3,2)}
     conflicts = set()
     label = 0
     for i in range(len(matr)):
-      # текущее значение ярлыка для внесения в матрицу ярлыков      
-      for j in range(len(matr[0])):
-        # Если значение не равно 0 создать ярлык в матрице ярлыков 
-        if matr[i][j]:
-          # Если значение слева ненулевое, ярлык = значение слева
-          if j > 0 and matr[i][j - 1] != 0:
-            labeled_matr[i][j] = labeled_matr[i][j-1]
-          # Если значение сверху ненулевое, ярлык = значение сверху
-          elif i > 0 and matr[i-1][j] != 0:
-            labeled_matr[i][j] = labeled_matr[i-1][j]
-          else:
-            label += 1
-            labeled_matr[i][j] = label
-          # Работа с конфликтами
-          if i > 0:
-            # Значение текущего ярлыка
-            current_label = labeled_matr[i][j]
-            # Значение ярлыка над текущим
-            above_label = labeled_matr[i - 1][j]
-            # Если текущй ярлык не равен ярлыку над ним, то создаем конфликт.
-            if above_label and above_label != current_label:
-              conflicts.add((max(above_label, current_label), min(above_label, current_label)))
-    print(labeled_matr)
-    print(conflicts)
+        # текущее значение ярлыка для внесения в матрицу ярлыков
+        for j in range(len(matr[0])):
+            # Если значение не равно 0 создать ярлык в матрице ярлыков
+            if matr[i][j]:
+                # Если значение слева ненулевое, ярлык = значение слева
+                if j > 0 and matr[i][j - 1] != 0:
+                    labeled_matr[i][j] = labeled_matr[i][j - 1]
+                # Если значение сверху ненулевое, ярлык = значение сверху
+                elif i > 0 and matr[i - 1][j] != 0:
+                    labeled_matr[i][j] = labeled_matr[i - 1][j]
+                else:
+                    label += 1
+                    labeled_matr[i][j] = label
+                # Работа с конфликтами
+                if i > 0:
+                    # Значение текущего ярлыка
+                    current_label = labeled_matr[i][j]
+                    # Значение ярлыка над текущим
+                    above_label = labeled_matr[i - 1][j]
+                    # Если текущй ярлык не равен ярлыку над ним, то создаем конфликт.
+                    if above_label and above_label != current_label:
+                        conflicts.add((max(above_label, current_label), min(above_label, current_label)))
+
     for i in range(len(labeled_matr)):
-      for j in range(len(labeled_matr[0])):
-        for conflict in sorted(conflicts, reverse=True):
-          if labeled_matr[i][j] == conflict[0]:
-            labeled_matr[i][j] = conflict[1]
-    print(labeled_matr)
-    
-
-    
-    return 5
-
-matr_test = [
-            [1, 1, 1, 1, 1],
-            [1, 0, 1, 0, 1],
-            [0, 1, 1, 0, 1],
-            [1, 0, 1, 1, 0],
-        ]
-
-calculate_selfmade(matr_test)
+        for j in range(len(labeled_matr[0])):
+            for conflict in sorted(conflicts, reverse=True):
+                if labeled_matr[i][j] == conflict[0]:
+                    labeled_matr[i][j] = conflict[1]
+    all_labels = set(itertools.chain.from_iterable(labeled_matr))
+    if 0 in all_labels:
+        return len(all_labels) - 1
+    else:
+        return len(all_labels)
