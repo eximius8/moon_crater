124db5cecea5166b2cec12140a6c1587bc099ae6
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-19 19:54:58 +0300

    Начал реализацию метода Row-by-Row в своем методе

--- main.py
+++ main.py
@@ -36,19 +36,29 @@ def calculate_selfmade(matr: list) -> int:
     Возвращает
       число найденных кратеров
     """
-    labeled_matr = [[],] * len(matr)
-    conflicts = []
+    labeled_matr = np.zeros( (len(matr), len(matr[0])))
+    print(labeled_matr)
+    conflicts = set()
     for i in range(len(matr)):
-      label = 0
+      label = 1
       for j in range(len(matr[0])):
+        print(i)
         if matr[i][j]:
           if j > 0 and not matr[i][j - 1]:
             label += 1
           labeled_matr[i][j] = label
+
           if i > 0 and labeled_matr[i - 1][j]:
             above = labeled_matr[i - 1][j]
-            conflicts.add((max(above, label), min(above, label)))
-        else:
-          labeled_matr[i][j] = 0
-    print(labeled_matr)
-   # return num
+            conflicts.add((max(above, label), min(above, label)))         
+        print(labeled_matr)
+    
+    return 5
+
+matr_test = [
+            [1, 1, 1],
+            [0, 0, 0],
+            [1, 0, 1],
+        ]
+
+calculate_selfmade(matr_test)
